732d2263a0272748d9267c522dc8942f208efc34
jennarin <jennarin.p@gmail.com>
2023-10-07 14:09:53 +0700

    thingspeak

--- fire_alarm_code.ino
+++ fire_alarm_code.ino
@@ -10,17 +10,21 @@ GND                   GND                     GND                 GND
  *
  */
 
-#include <TridentTD_LineNotify.h>  //thank you Mr.TridentTD https://github.com/TridentTD/TridentTD_LineNotify
+#include <TridentTD_LineNotify.h> //thank you Mr.TridentTD https://github.com/TridentTD/TridentTD_LineNotify
 #include <ESP8266WiFi.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
-#define ssid "xanny"                                              //you wifi router
-#define pass "6410110082"                                         //password of wifi
-#define LINE_TOKEN "shhXvGPgs1oteDTfteMWaqRix3aCaKO6DttOateAp15"  //line token at https://notify-bot.line.me/en/
+#define ssid "xanny"                                             // you wifi router
+#define pass "6410110082"                                        // password of wifi
+#define LINE_TOKEN "shhXvGPgs1oteDTfteMWaqRix3aCaKO6DttOateAp15" // line token at https://notify-bot.line.me/en/
 #define BUZZER_PIN D6
-#define SPRINKLER_START_DELAY 3000  //5 seconds
-#define SPRINKLER_ON_TIME 3000      //3 seconds Sprinkler on time
+#define SPRINKLER_START_DELAY 3000 // 5 seconds
+#define SPRINKLER_ON_TIME 3000     // 3 seconds Sprinkler on time
+
+const char *host = "api.thingspeak.com"; // Host ของ thingspeak ไม่ต้องแก้ไข
+const char *api = "DDTIP8UQMJA5G9TW";
+int status = 0;
 
 TridentTD_LineNotify myLINE(LINE_TOKEN);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -34,7 +38,6 @@ int RELAY_PIN = D4;
 int SMOKE_PIN = 10;
 int FLAME_PIN = D7;
 
-
 int isFlame = HIGH;
 int isSmoke = HIGH;
 
@@ -42,11 +45,8 @@ bool smoke1 = 0;
 bool smoke2 = 0;
 bool flame1 = 0;
 
-void flameInterruptHandler()
+void setup()
 {
-  
-}
-void setup() {
   Serial.begin(115200);
   pinMode(SMOKE_PIN, INPUT);
   pinMode(FLAME_PIN, INPUT_PULLUP);
@@ -55,21 +55,35 @@ void setup() {
   pinMode(LED_OK, OUTPUT);
   pinMode(LED_ALERT, OUTPUT);
 
-  lcd.init();  // Initialize the LCD
+  lcd.init(); // Initialize the LCD
   lcd.begin(16, 2);
   lcd.backlight();
-  lcd.setCursor(4, 0);
+  lcd.setCursor(0, 0);
   lcd.print("STATUS OK");
 
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, pass);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  // }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
   digitalWrite(LED_OK, HIGH);
   digitalWrite(LED_ALERT, LOW);
   digitalWrite(RELAY_PIN, LOW);
 }
 
-void loop() {                     // Wait for 1 second
-  digitalWrite(BUZZER_PIN, LOW);  // Turn the buzzer off
+void loop()
+{                                // Wait for 1 second
+  digitalWrite(BUZZER_PIN, LOW); // Turn the buzzer off
   smokeRead();
   smokeDetect();
   flameDetect();
@@ -77,23 +91,28 @@ void loop() {                     // Wait for 1 second
   Serial.println(smoke1);
   Serial.println(smoke2);
 
-  if (flame1 == 1 || smoke1 == 1 || smoke2 == 1) {
-    myLINE.notify("Fire! Fire! Fire!");  //  Messenger to line notification
+  if (flame1 == 1 || smoke1 == 1 || smoke2 == 1)
+  {
+    myLINE.notify("Fire! Fire! Fire!"); //  Messenger to line notification
 
     digitalWrite(BUZZER_PIN, HIGH);
     Serial.println("Warnig, Warning, Warning");
 
-    lcd.setCursor(3, 0);
+    lcd.setCursor(0, 0);
     lcd.print("FIRE DETECTED!!");
     digitalWrite(LED_OK, LOW);
     digitalWrite(LED_ALERT, HIGH);
 
-    if (millis() - previousTime > SPRINKLER_START_DELAY) {
+    if (millis() - previousTime > SPRINKLER_START_DELAY)
+    {
       digitalWrite(RELAY_PIN, HIGH);
       delay(SPRINKLER_ON_TIME);
     }
     delay(3000);
-  } else {
+    status = 1;
+  }
+  else
+  {
     analogWrite(BUZZER_PIN, LOW);
     digitalWrite(RELAY_PIN, LOW);
     digitalWrite(LED_OK, HIGH);
@@ -101,49 +120,100 @@ void loop() {                     // Wait for 1 second
     lcd.setCursor(0, 0);
     lcd.print("    STATUS OK    ");
     previousTime = millis();
+    status = 0;
   }
-
-
   delay(2000);
+  ThingSpeak();
 }
 
+void ThingSpeak()
+{
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort))
+  {
+    Serial.println("connection failed");
+    return;
+  }
+
+  String url = "/update?api_key=";
+  url += api;
+  url += "&field1=";
+  url += status;
+  if (status == 1)
+  {
+    url += "&field2=";
+    url += status;
+  }
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+  delay(10);
+
+  // Read all the lines of the reply from server and print them to Serial
+  while (client.available())
+  {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  Serial.println();
+  Serial.println("closing connection");
+}
 /////////////////////////////////////////////////
 
-void smokeDetect() {
+void smokeDetect()
+{
   isSmoke = digitalRead(SMOKE_PIN);
-  if (isSmoke == LOW) {
+  if (isSmoke == LOW)
+  {
     smoke1 = 1;
     Serial.println("SMOKE, SMOKE, SMOKE");
-  } else {
+  }
+  else
+  {
     smoke1 = 0;
     Serial.println("NO SMOKE");
   }
 }
 
-void flameDetect() {
+void flameDetect()
+{
   isFlame = digitalRead(FLAME_PIN);
-  if (isFlame == LOW) {
+  if (isFlame == LOW)
+  {
     flame1 = 1;
     Serial.println("FLAME, FLAME, FLAME");
     Serial.println(BUZZER_PIN);
     digitalWrite(BUZZER_PIN, HIGH);
-  } else {
+  }
+  else
+  {
     flame1 = 0;
     digitalWrite(RELAY_PIN, LOW);
     Serial.println("no flame");
   }
 }
 
-void smokeRead() {
+void smokeRead()
+{
   sensorValue = analogRead(A0);
   Serial.println(sensorValue);
-  if (sensorValue > 380) {
+  if (sensorValue > 380)
+  {
     smoke2 = 1;
     Serial.println("WARNING SMOKE TOO HIGH");
 
     lcd.setCursor(3, 0);
     lcd.print("SMOKE DETECTED!!");
-  } else {
+  }
+  else
+  {
     smoke2 = 0;
     Serial.println("NO SMOKE");
   }
